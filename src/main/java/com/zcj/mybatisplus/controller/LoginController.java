d866c5be01c570538fd1a8a799696fd56681af4b
Chuickies <1601459351@qq.com>
2020-03-15 22:28:56 +0800

    update

--- src/main/java/com/zcj/mybatisplus/controller/LoginController.java
+++ src/main/java/com/zcj/mybatisplus/controller/LoginController.java
@@ -6,6 +6,7 @@ import com.zcj.mybatisplus.entity.result.ResultInfo;
 import com.zcj.mybatisplus.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
